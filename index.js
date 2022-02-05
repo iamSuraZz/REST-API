1597e68ed8d23c4ed0f128f7e1fa41d3d6bba73e
SuraZz007 <surajdev9886@gmail.com>
2022-02-05 16:47:24 +0530

    GET demo data user as response

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express();
 let router = express.Router();
 app.use("/api/", router);
 
+const users = [
+  { name: "John Does", age: 36, gender: "Male" },
+  { name: "Krish", age: 34, gender: "Male" },
+  { name: "Mary", age: 24, gender: "Female" },
+];
+
 // Read Operation
 router.get("/", function (req, res, next) {
-  res.send("John Smith");
+  res.send(users);
 });
 
 app.listen(5000, function () {
